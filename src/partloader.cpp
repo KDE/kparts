481299d05e5c65a623b23785c9dd15d5f8982eb5
Laurent Montel <montel@kde.org>
2021-11-03 07:48:05 +0100

    Remove duplicate includes from .cpp/.h

--- src/partloader.cpp
+++ src/partloader.cpp
@@ -10,8 +10,6 @@
 #include "kparts_logging.h"
 
 #include <KConfigGroup>
-#include <KLocalizedString>
-#include <KPluginFactory>
 #include <KPluginLoader>
 #include <KService>
 #include <KSharedConfig>
