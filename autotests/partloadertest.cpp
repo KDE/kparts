adf5adc4c75c7f4f4b6db28c47ec8ed10fab3588
Alexander Lohnau <alexander.lohnau@gmx.de>
2023-09-04 16:42:42 +0200

    Clean up PartLoaderTest
    Some parts can be simplified

--- autotests/partloadertest.cpp
+++ autotests/partloadertest.cpp
@@ -15,6 +15,8 @@
 class PartLoaderTest : public QObject
 {
     Q_OBJECT
+private:
+    const QString m_plainTextMimetype = QStringLiteral("text/plain");
 private Q_SLOTS:
     void initTestCase()
     {
@@ -35,17 +37,9 @@ private Q_SLOTS:
 
     void shouldListParts()
     {
-        // GIVEN
-        const QString mimeType = QStringLiteral("text/plain");
+        const QList<KPluginMetaData> plugins = KParts::PartLoader::partsForMimeType(m_plainTextMimetype);
 
-        // WHEN
-        const QList<KPluginMetaData> plugins = KParts::PartLoader::partsForMimeType(mimeType);
-
-        // THEN
         QVERIFY(!plugins.isEmpty());
-        //        for (const KPluginMetaData &metaData : plugins) {
-        //            qDebug() << metaData.fileName() << metaData.pluginId();
-        //        }
         QCOMPARE(plugins.at(0).pluginId(), QStringLiteral("notepadpart")); // basename of plugin
         const QString fileName = plugins.at(0).fileName();
         QVERIFY2(fileName.contains(QLatin1String("notepadpart")), qPrintable(fileName));
@@ -53,54 +47,39 @@ private Q_SLOTS:
 
     void shouldLoadPlainTextPart()
     {
-        // GIVEN
-        const QString mimeType = QStringLiteral("text/plain");
         const QString testFile = QFINDTESTDATA("partloadertest.cpp");
         QVERIFY(!testFile.isEmpty());
         QWidget parentWidget;
-        QString errorString;
 
-        // WHEN
-        auto res = KParts::PartLoader::instantiatePartForMimeType<KParts::ReadOnlyPart>(mimeType, &parentWidget, this);
+        auto res = KParts::PartLoader::instantiatePartForMimeType<KParts::ReadOnlyPart>(m_plainTextMimetype, &parentWidget, this);
 
-        // THEN
         QVERIFY(res);
-        QCOMPARE(errorString, QString());
+        QCOMPARE(res.errorString, QString());
         QCOMPARE(res.plugin->metaObject()->className(), "NotepadPart");
         QVERIFY(res.plugin->openUrl(QUrl::fromLocalFile(testFile)));
     }
 
     void shouldHandleNoPartError()
     {
-        // GIVEN
         // can't use an unlikely mimetype here, okteta is associated with application/octet-stream :-)
         const QString mimeType = QStringLiteral("does/not/exist");
         QWidget parentWidget;
 
-        // WHEN
-        const KPluginFactory::Result result = KParts::PartLoader::instantiatePartForMimeType<KParts::ReadOnlyPart>(mimeType, &parentWidget, this, {});
-        KParts::ReadOnlyPart *part = result.plugin;
-        QString errorString = result.errorString;
+        const KPluginFactory::Result result = KParts::PartLoader::instantiatePartForMimeType<KParts::ReadOnlyPart>(mimeType, &parentWidget, this);
 
-        // THEN
-        QVERIFY2(!part, part ? part->metaObject()->className() : nullptr);
-        QCOMPARE(errorString, QStringLiteral("No part was found for mimeType does/not/exist"));
+        QVERIFY2(!result, result ? result.plugin->metaObject()->className() : nullptr);
+        QCOMPARE(result.errorString, QStringLiteral("No part was found for mimeType does/not/exist"));
     }
 
     void shouldInstantiatePart()
     {
-        // GIVEN
         const KPluginMetaData md(QStringLiteral("kf6/parts/notepadpart"));
         QVERIFY(md.isValid());
 
         QWidget parentWidget;
+        const KPluginFactory::Result result = KParts::PartLoader::instantiatePart<KParts::ReadOnlyPart>(md, &parentWidget, this);
 
-        // WHEN
-        const KPluginFactory::Result result = KParts::PartLoader::instantiatePart<KParts::ReadOnlyPart>(md, &parentWidget, this, {});
-
-        // THEN
         QVERIFY(result);
-        QVERIFY(result.plugin);
         QCOMPARE(result.plugin->metaObject()->className(), "NotepadPart");
     }
 };
